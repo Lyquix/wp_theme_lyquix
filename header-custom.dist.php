903dd9dfa38524ff01cd099e8486f8c6032ceb86
Ruben Reyes <ruben.reyes@lyquix.com>
2016-10-31 16:23:46 -0400

    Add favicon code to theme

--- header-custom.dist.php
+++ header-custom.dist.php
@@ -90,6 +90,49 @@ if(get_theme_mod('mobiledetect_method', 'php') == 'php') {
 	?>
 	<script src="<?php echo $tmpl_url; ?>/js/lyquix<?php echo get_theme_mod('non_min_js') ? '' : '.min'; ?>.js?v=<?php echo date("YmdHis", filemtime($tmpl_path . '/js/lyquix.'.get_theme_mod('non_min_js')? '' : '.min'.'js')); ?>"></script>
 	<?php 
+	// use http://www.favicon-generator.org/ to generate all these versions
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-57x57.png')): ?>
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-57x57.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-60x60.png')): ?>
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-60x60.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-72x72.png')): ?>
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-72x72.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-76x76.png')): ?>
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-76x76.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-114x114.png')): ?>
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-114x114.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-120x120.png')): ?>
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-120x120.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-144x144.png')): ?>
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-144x144.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-152x152.png')): ?>
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-152x152.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-180x180.png')): ?>
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-180x180.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/android-icon-192x192.png')): ?>
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $tmpl_url; ?>/images/favicon/android-icon-192x192.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/favicon.ico')): ?>
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo $tmpl_url; ?>/images/favicon/favicon.ico">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/favicon-32x32.png')): ?>
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $tmpl_url; ?>/images/favicon/favicon-32x32.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/favicon-96x96.png')): ?>
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php echo $tmpl_url; ?>/images/favicon/favicon-96x96.png">
+	<?php endif;
+	if(file_exists($tmpl_path . '/images/favicon/favicon-16x16.png')): ?>
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tmpl_url; ?>/images/favicon/favicon-16x16.png">
+	<?php endif;
 	$analytics_account = get_theme_mod('analytics_account');
 	if (!empty($analytics_account)):
 		echo "<script>
