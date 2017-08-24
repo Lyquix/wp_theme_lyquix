64e7ff0cbf9e8cd233f8d31396b7bd02b54f2a7a
Matt Austin <matt.austin@lyquix.com>
2017-08-24 13:41:08 -0400

    Change: body-top

--- php/body-top.inc.php
+++ php/body-top.inc.php
@@ -1,10 +1,11 @@
 <script>
 lqx.bodyScreenSize();
 lqx.vars.siteURL = '<?php echo $site_abs_url; ?>';
-lqx.vars.tmplURL = '<?php echo $site_abs_url; ?>';
-<?php if(get_theme_mod('mobiledetect_method', 'php') == 'js'): ?>lqx.mobileDetect = lqx.mobileDetect();
-<?php endif;
+lqx.vars.tmplURL = '<?php echo $tmpl_url; ?>';
+<?php 
+if(get_theme_mod('mobiledetect_method', 'php') == 'js') echo "lqx.mobileDetect = lqx.mobileDetect();\n"; 
 if(get_theme_mod('mobiledetect_method', 'php') == 'php'){
 	echo 'lqx.mobileDetect = {mobile: ' . ($mobile ? 'true' : 'false') . ',phone: ' . ($phone ? 'true' : 'false') . ',tablet: ' . ($tablet ? 'true' : 'false') . "};\n";
-}?>
+}
+?>
 </script>
