dbc401b04d5416626e948ae4ddc044079a77910b
Matt Austin <matt.austin@lyquix.com>
2017-08-24 13:41:56 -0400

    Update: Favicon declarations

--- php/favicon.inc.php
+++ php/favicon.inc.php
@@ -1,30 +1,12 @@
-<?php if(file_exists($tmpl_path . '/images/favicon/apple-icon-57x57.png')): ?>
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-57x57.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-60x60.png')): ?>
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-60x60.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-72x72.png')): ?>
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-72x72.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-76x76.png')): ?>
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-76x76.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-114x114.png')): ?>
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-114x114.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-120x120.png')): ?>
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-120x120.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-144x144.png')): ?>
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-144x144.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-152x152.png')): ?>
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-152x152.png">
-<?php endif;
-if(file_exists($tmpl_path . '/images/favicon/apple-icon-180x180.png')): ?>
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $tmpl_url; ?>/images/favicon/apple-icon-180x180.png">
-<?php endif;
+<?php
+// use http://www.favicon-generator.org/ to generate all these versions
+$favicons_sizes = [57,60,72,76,114,120,144,152,180];
+foreach($favicons_sizes as $favicon_size) {
+	$favicon_size = $favicon_size . 'x' . $favicon_size;
+	if(file_exists($tmpl_path . '/images/favicon/apple-icon-' . $favicon_size . '.png')) {
+		echo '<link rel="apple-touch-icon" sizes="' . $favicon_size . '" href="' . $tmpl_url . '/images/favicon/apple-icon-' . $favicon_size . '.png">' . "\n" ;
+	}
+}
 if(file_exists($tmpl_path . '/images/favicon/android-icon-192x192.png')): ?>
 <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo $tmpl_url; ?>/images/favicon/android-icon-192x192.png">
 <?php endif;
@@ -39,4 +21,4 @@ if(file_exists($tmpl_path . '/images/favicon/favicon-96x96.png')): ?>
 <?php endif;
 if(file_exists($tmpl_path . '/images/favicon/favicon-16x16.png')): ?>
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $tmpl_url; ?>/images/favicon/favicon-16x16.png">
-<?php endif;
+<?php endif; ?>
