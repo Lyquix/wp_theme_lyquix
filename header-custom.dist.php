727f6f840a5959793f43a6e8fb6bdcb8259b52b7
Matt Austin <matt.austin@lyquix.com>
2017-08-24 13:39:08 -0400

    Remove: pingback from header

--- header-custom.dist.php
+++ header-custom.dist.php
@@ -5,11 +5,6 @@ include('php/head-pre.inc.php');
 <head>
 	<?php
 	wp_head();
-	?>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif;
 	include('php/head-top.inc.php');
 	include('php/css.inc.php');
 	include('php/js.inc.php');
