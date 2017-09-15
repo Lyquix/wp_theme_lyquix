ae8f8c3054dc2c5a0c799c3c38be3a98a81255d3
Matt Austin <matt.austin@lyquix.com>
2017-09-15 16:04:46 -0400

    Change: Function used to determine locale

--- php/body-bottom.inc.php
+++ php/body-bottom.inc.php
@@ -13,13 +13,14 @@
 if(get_theme_mod('ie8_alert',1)): ?>
 <!--[if lte IE 8]>
 <link href="<?php echo $tmpl_url; ?>/css/ie8-alert.css" rel="stylesheet" />
-<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a></div>
+<div class="ie8-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo get_locale(); ?>">please upgrade to an up-to-date browser.</a></div>
 <![endif]-->
 <?php endif;
 if(get_theme_mod('ie9_alert',1)): ?>
 <!--[if IE 9]>
 <link href="<?php echo $tmpl_url; ?>/css/ie9-alert.css" rel="stylesheet" />
-<div class="ie9-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo $this->language; ?>">please upgrade to an up-to-date browser.</a><i></i></div>
+<div class="ie9-alert">You are using an unsupported version of Internet Explorer. To ensure security, performance, and full functionality, <a href="http://browsehappy.com/?locale=<?php echo get_locale(); ?>">please upgrade to an up-to-date browser.</a><i></i></div>
 <script>jQuery('.ie9-alert i').click(function(){jQuery('.ie9-alert').hide();});</script>
+<![endif]-->
 <![endif]-->
 <?php endif; ?>
