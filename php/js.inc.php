fd0bb6df082a244fe302f3967c5a296c040682d5
rubenreyes2000 <ruben.reyes@lyquix.com>
2020-07-16 11:04:06 -0400

    Fix: wrong parameter name used for Google Analytics account

--- php/js.inc.php
+++ php/js.inc.php
@@ -9,7 +9,7 @@
  * @license     GNU General Public License version 2 or later
  * @link        https://github.com/Lyquix/tpl_lyquix
  */
- 
+
 if(get_theme_mod('angularjs', 0)): ?>
 <script src="<?php echo $cdnjs_url; ?>angular.js/1.6.1/angular<?php echo get_theme_mod('non_min_js') ? '' : '.min'; ?>.js"></script>
 <?php endif;
@@ -34,13 +34,13 @@ foreach($add_js_libraries as $jsurl) {
 	if($jsurl) {
 		echo '<script src="' . $jsurl . '"></script>';
 	}
-} 
+}
 ?>
 <script src="<?php echo $tmpl_url; ?>/js/lyquix<?php echo get_theme_mod('non_min_js') ? '' : '.min'; ?>.js?v=<?php echo date("YmdHis", filemtime($tmpl_path . '/js/lyquix' . (get_theme_mod('non_min_js') ? '' : '.min') . '.js')); ?>"></script>
 <script src="<?php echo $tmpl_url; ?>/js/scripts<?php echo get_theme_mod('non_min_js') ? '' : '.min'; ?>.js?v=<?php echo date("YmdHis", filemtime($tmpl_path . '/js/scripts' . (get_theme_mod('non_min_js') ? '' : '.min') . '.js')); ?>"></script>
 <script>lqx.setOptions({
-	bodyScreenSize: {min: <?php echo get_theme_mod('min_screen', 0); ?>, max: <?php echo get_theme_mod('max_screen', 4); ?>}<?php if(get_theme_mod('ga_account')) : ?>,
-	ga: {createParams: {default: {trackingId: '<?php echo get_theme_mod('ga_account'); ?>', cookieDomain: 'auto'}}}<?php endif; ?>
+	bodyScreenSize: {min: <?php echo get_theme_mod('min_screen', 0); ?>, max: <?php echo get_theme_mod('max_screen', 4); ?>}<?php if(get_theme_mod('analytics_account')) : ?>,
+	ga: {createParams: {default: {trackingId: '<?php echo get_theme_mod('analytics_account'); ?>', cookieDomain: 'auto'}}}<?php endif; ?>
 });</script>
 <?php if(get_theme_mod('lqx_options', '{}') != '{}') : ?>
 <script>lqx.setOptions(<?php echo get_theme_mod('lqx_options', '{}'); ?>);</script>
