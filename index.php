3200e6b25b9d497fbc99b4719f69560c280b1a46
rubenreyes2000 <ruben.reyes@lyquix.com>
2023-10-03 19:50:27 -0400

    Add STOP! to various files

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
  * @link        https://github.com/Lyquix/wp_theme_lyquix
  */
 
+//    .d8888b. 88888888888 .d88888b.  8888888b.   888
+//   d88P  Y88b    888    d88P" "Y88b 888   Y88b  888
+//   Y88b.         888    888     888 888    888  888
+//    "Y888b.      888    888     888 888   d88P  888
+//       "Y88b.    888    888     888 8888888P"   888
+//         "888    888    888     888 888         Y8P
+//   Y88b  d88P    888    Y88b. .d88P 888          "
+//    "Y8888P"     888     "Y88888P"  888         888
+//
+//  DO NOT MODIFY THIS FILE!
+//  If you want to customize the theme, create a file called custom.php
+
 if(file_exists(get_template_directory(). '/custom.php')) :
 	require get_template_directory() . '/custom.php';
 else :
