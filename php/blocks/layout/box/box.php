cf6f96f5f3628cffe4622ad1449666741f0cf082
rubenreyes2000 <ruben.reyes@lyquix.com>
2023-11-30 15:11:57 -0500

    Remove print_r from box.php
    (cherry picked from commit e0af316dac317631850c41ce92b7a0700d87b38c)

--- php/blocks/layout/box/box.php
+++ php/blocks/layout/box/box.php
@@ -1,8 +1,6 @@
 <?php
 
 require(get_stylesheet_directory() . '/php/blocks/layout/layout.php');
-
-echo '<pre>' . print_r($block, true) . '</pre>';
 ?>
 <div id="<?= esc_attr($block['anchor']) ?>" class="box-l <?= $classes ?>">
 	<InnerBlocks />
