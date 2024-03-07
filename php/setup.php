d4c2cd4b35bb89d5ef0bc5f25f4332d724f478f4
rubenreyes2000 <ruben.reyes@lyquix.com>
2024-03-07 11:43:06 -0500

    Required plugins alert is dismissed for 1 day
    (cherry picked from commit 2fe39794dcb7d2cd23645cc610188f206f2d9482)

--- php/setup.php
+++ php/setup.php
@@ -116,81 +116,99 @@ function theme_setup() {
 
 	// Add alerts for required plugins
 	if (get_theme_mod('feat_required_plugins_alert', '1') === '1') {
-		add_action('admin_init', function () {
-			$required_plugins = [
-				'aryo-activity-log/aryo-activity-log.php' => 'Activity Log',
-				'advanced-custom-fields-pro/acf.php' => 'Advanced Custom Fields PRO',
-				'acf-extended-pro/acf-extended.php' => 'Advanced Custom Fields: Extended PRO',
-				'admin-menu-editor-pro/menu-editor.php' => 'Admin Menu Editor Pro',
-				'gravityforms/gravityforms.php' => 'Gravity Forms',
-				'post-smtp/postman-smtp.php' => 'Post SMTP',
-				'redirection/redirection.php' => 'Redirection',
-				'wordpress-seo/wp-seo.php' => 'Yoast SEO',
-				'duplicate-post/duplicate-post.php' => 'Yoast Duplicate Post',
-				'simple-custom-post-order/simple-custom-post-order.php' => 'Simple Custom Post Order',
-				'tinymce-advanced/tinymce-advanced.php' => 'Advanced Editor Tools',
-				'html-editor-syntax-highlighter/html-editor-syntax-highlighter.php' => 'HTML Editor Syntax Highlighter',
-				'ewww-image-optimizer/ewww-image-optimizer.php' => 'EWWW Image Optimizer',
-				'w3-total-cache/w3-total-cache.php' => 'W3 Total Cache',
-				'wordfence/wordfence.php' => 'Wordfence',
-			];
+		// Check plugins and display alert
+		if (get_transient('dismissed_required_plugins_alert') === false) {
+			add_action('admin_init', function () {
+				$required_plugins = [
+					'aryo-activity-log/aryo-activity-log.php' => 'Activity Log',
+					'advanced-custom-fields-pro/acf.php' => 'Advanced Custom Fields PRO',
+					'acf-extended-pro/acf-extended.php' => 'Advanced Custom Fields: Extended PRO',
+					'admin-menu-editor-pro/menu-editor.php' => 'Admin Menu Editor Pro',
+					'gravityforms/gravityforms.php' => 'Gravity Forms',
+					'post-smtp/postman-smtp.php' => 'Post SMTP',
+					'redirection/redirection.php' => 'Redirection',
+					'wordpress-seo/wp-seo.php' => 'Yoast SEO',
+					'duplicate-post/duplicate-post.php' => 'Yoast Duplicate Post',
+					'simple-custom-post-order/simple-custom-post-order.php' => 'Simple Custom Post Order',
+					'tinymce-advanced/tinymce-advanced.php' => 'Advanced Editor Tools',
+					'html-editor-syntax-highlighter/html-editor-syntax-highlighter.php' => 'HTML Editor Syntax Highlighter',
+					'ewww-image-optimizer/ewww-image-optimizer.php' => 'EWWW Image Optimizer',
+					'w3-total-cache/w3-total-cache.php' => 'W3 Total Cache',
+					'wordfence/wordfence.php' => 'Wordfence',
+				];
 
-			$premium_plugins = [
-				'advanced-custom-fields-pro/acf.php' => 'https://www.advancedcustomfields.com/pro/',
-				'acf-extended-pro/acf-extended.php' => 'https://www.acf-extended.com/',
-				'admin-menu-editor-pro/menu-editor.php' => 'https://adminmenueditor.com/',
-				'gravityforms/gravityforms.php' => 'https://www.gravityforms.com/'
-			];
+				$premium_plugins = [
+					'advanced-custom-fields-pro/acf.php' => 'https://www.advancedcustomfields.com/pro/',
+					'acf-extended-pro/acf-extended.php' => 'https://www.acf-extended.com/',
+					'admin-menu-editor-pro/menu-editor.php' => 'https://adminmenueditor.com/',
+					'gravityforms/gravityforms.php' => 'https://www.gravityforms.com/'
+				];
 
-			// Retrieve all installed plugins' data
-			$all_plugins = get_plugins();
-			$not_installed = [];
-			$not_active = [];
+				// Retrieve all installed plugins' data
+				$all_plugins = get_plugins();
+				$not_installed = [];
+				$not_active = [];
 
-			foreach ($required_plugins as $plugin_path => $plugin_name) {
-				// Check if the plugin is installed
-				if (isset($all_plugins[$plugin_path])) {
-					// Check if the plugin is active
-					if (!is_plugin_active($plugin_path)) {
-						$not_active[] = $plugin_path;
+				foreach ($required_plugins as $plugin_path => $plugin_name) {
+					// Check if the plugin is installed
+					if (isset($all_plugins[$plugin_path])) {
+						// Check if the plugin is active
+						if (!is_plugin_active($plugin_path)) {
+							$not_active[] = $plugin_path;
+						}
+					} else {
+						// Plugin is not installed
+						$not_installed[] = $plugin_path;
 					}
-				} else {
-					// Plugin is not installed
-					$not_installed[] = $plugin_path;
 				}
-			}
 
-			if (count($not_installed) || count($not_active)) {
-				add_action('admin_notices', function () use ($not_active, $not_installed, $required_plugins, $premium_plugins) {
-					echo '<div class="notice notice-error is-dismissible"><p><strong style="font-size: 1.25em;">Required Plugins</strong><br>';
-					if (count($not_installed)) {
-						$html = [];
-						foreach ($not_installed as $plugin_path) {
-							if (array_key_exists($plugin_path, $premium_plugins)) {
-								$install_url = $premium_plugins[$plugin_path];
-								$target = '_blank';
-							} else {
-								$plugin_slug = explode('/', $plugin_path)[0];
-								$install_url = wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=' . $plugin_slug), 'install-plugin_' . $plugin_slug);
-								$target = '';
+				if (count($not_installed) || count($not_active)) {
+					add_action('admin_notices', function () use ($not_active, $not_installed, $required_plugins, $premium_plugins) {
+						echo '<div class="notice notice-error is-dismissible required-plugins-alert"><p><strong style="font-size: 1.25em;">Required Plugins</strong><br>';
+						if (count($not_installed)) {
+							$html = [];
+							foreach ($not_installed as $plugin_path) {
+								if (array_key_exists($plugin_path, $premium_plugins)) {
+									$install_url = $premium_plugins[$plugin_path];
+									$target = '_blank';
+								} else {
+									$plugin_slug = explode('/', $plugin_path)[0];
+									$install_url = wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=' . $plugin_slug), 'install-plugin_' . $plugin_slug);
+									$target = '';
+								}
+								$html[] = sprintf('<a href="%s" target="%s">%s</a>', $install_url, $target, $required_plugins[$plugin_path]);
 							}
-							$html[] = sprintf('<a href="%s" target="%s">%s</a>', $install_url, $target, $required_plugins[$plugin_path]);
+							echo  '<strong>Install:</strong> ' . implode(' | ', $html) . '<br>';
 						}
-						echo  '<strong>Install:</strong> ' . implode(' | ', $html) . '<br>';
-					}
-					if (count($not_active)) {
-						$html = [];
-						foreach ($not_active as $plugin_path) {
-							$plugin_slug = basename($plugin_path);
-							$activate_url = wp_nonce_url('plugins.php?action=activate&amp;plugin=' . urlencode($plugin_path), 'activate-plugin_' . $plugin_path);
-							$html[] = sprintf('<a href="%s">%s</a>', $activate_url, $required_plugins[$plugin_path]);
+						if (count($not_active)) {
+							$html = [];
+							foreach ($not_active as $plugin_path) {
+								$plugin_slug = basename($plugin_path);
+								$activate_url = wp_nonce_url('plugins.php?action=activate&amp;plugin=' . urlencode($plugin_path), 'activate-plugin_' . $plugin_path);
+								$html[] = sprintf('<a href="%s">%s</a>', $activate_url, $required_plugins[$plugin_path]);
+							}
+							echo '<strong>Activate:</strong> ' . implode(' | ', $html);
 						}
-						echo '<strong>Activate:</strong> ' . implode(' | ', $html);
-					}
-					echo '</p></div>';
-				});
-			}
-		});
+						echo '</p></div>';
+
+						// Add script to handle the notice dismissal
+						echo '<script>
+							jQuery(document).on("click", ".notice-dismiss", function() {
+								jQuery.ajax({ url: ajaxurl,
+								data: { action: "dismiss_required_plugins_alert" } });
+							});
+						</script>';
+					});
+				}
+			});
+
+			// Handle the dismiss AJAX request
+			add_action('wp_ajax_dismiss_required_plugins_alert', function () {
+				// Set the transient to expire after 1 day
+				set_transient('dismissed_required_plugins_alert', true, DAY_IN_SECONDS);
+				wp_die();
+			});
+		}
 	}
 
 	// Add user management capabilities to editor user role
