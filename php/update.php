0c4a8d3723be4ca257a89733d1e46b49cd01c2f0
rubenreyes2000 <ruben.reyes@lyquix.com>
2024-03-06 21:13:46 -0500

    Update theme update checker to cache github response for 1 day

--- php/update.php
+++ php/update.php
@@ -29,22 +29,36 @@ function theme_update_notice() {
 	$theme = wp_get_theme();
 	$theme_version = $theme->get('Version');
 
-	// Check in Github the latest released version of the theme
-	$github_url = 'https://api.github.com/repos/Lyquix/wp_theme_lyquix/releases/latest';
-	$github_response = wp_remote_get($github_url);
-	if (!is_wp_error($github_response)) {
-		$github_body = wp_remote_retrieve_body($github_response);
-		$github_data = json_decode($github_body);
-
-		// If the latest version is newer than the current version, show the update notice
-		if (version_compare($github_data->tag_name, $theme_version, '>')) {
-			// Add update notice
-			echo '<div class="notice notice-update is-dismissible">';
-			echo '<p>Lyquix Theme has an update available! <a href="https://github.com/Lyquix/wp_theme_lyquix/releases/latest">Download the Latest Release</a></p>';
-			echo '</div>';
+	// Check if we have a cached version of the latest release
+	$tag_name = '';
+	if (file_exists(__DIR__ . '/update.json')) {
+		if (filemtime(__DIR__ . '/update.json') > strtotime('-1 day')) {
+			$github_data = json_decode(file_get_contents(__DIR__ . '/update.json'), true);
+			$tag_name = $github_data['tag_name'];
 		}
 	}
 
+	if (!$tag_name) {
+		// Check in Github the latest released version of the theme
+		$github_url = 'https://api.github.com/repos/Lyquix/wp_theme_lyquix/releases/latest';
+		$github_response = wp_remote_get($github_url);
+		if (!is_wp_error($github_response)) {
+			$github_body = wp_remote_retrieve_body($github_response);
+			$github_data = json_decode($github_body, true);
+			if (array_key_exists('tag_name', $github_data)) {
+				$tag_name = $github_data['tag_name'];
+				file_put_contents(__DIR__ . '/update.json', json_encode($github_data, JSON_PRETTY_PRINT));
+			}
+		}
+	}
+
+	// If the latest version is newer than the current version, show the update notice
+	if ($tag_name && version_compare($tag_name, $theme_version, '>')) {
+		// Add update notice
+		echo '<div class="notice notice-update is-dismissible">';
+		echo '<p>Lyquix Theme has an update available! <a href="https://github.com/Lyquix/wp_theme_lyquix/releases/latest">Download the Latest Release</a></p>';
+		echo '</div>';
+	}
 }
 
 // Check if the user is an admin viewing a dashboard page
