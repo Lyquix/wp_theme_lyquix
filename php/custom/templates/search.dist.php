4272ad55e643c68485ced118b1518c680ccfbaf3
caiuscostin <caius.costin@lyquix.com>
2024-11-08 14:02:15 +0200

    Remove styles from search result

--- php/custom/templates/search.dist.php
+++ php/custom/templates/search.dist.php
@@ -58,6 +58,8 @@
 					} else {
 						// Get the content
 						$content = apply_filters('the_content', get_the_content());
+						// Remove styles
+						$content = preg_replace('/<style>.*?<\/style>/s', '', $content);
 						// Remove shortcodes and strip tags
 						$content = strip_tags(strip_shortcodes($content));
 						// Limit the content to 300 characters and trim
