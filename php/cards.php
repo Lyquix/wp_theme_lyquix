aa315ad8ebd5d3f2dd1ad56d0662d4ba2a628ce1
ionutpopean314 <ionut.popean@lyquix.com>
2024-12-01 17:36:21 +0200

    Cards fix + grid container on content wrapper

--- php/cards.php
+++ php/cards.php
@@ -136,7 +136,7 @@ function process_wp_posts($posts, $fields_map, $fields_values) {
 
     if(is_array($posts)) {
         foreach ($posts as $i => $post) {
-            $processed_post = isset($fields_values[$i]) ? process_wp_post($post, $fields_map, $fields_values[$i]) : null;
+            $processed_post = process_wp_post($post, $fields_map, $fields_values[$i] ?? null);
             if ($processed_post) $processed_posts[] = $processed_post;
         }
     }
