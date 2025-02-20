0103c9bce1c07b49324c787e0807d014fb17acbe
maustin92 <matt.austin@lyquix.com>
2025-02-20 08:55:22 -0500

    Change: add missing "STOP" message to default template file and add random hash to individual maps

--- php/blocks/map/default.tmpl.php
+++ php/blocks/map/default.tmpl.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * default.tmpl.php - Default template for Lyquix map block
+ *
+ * @version     3.1.0
+ * @package     wp_theme_lyquix
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2024 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/wp_theme_lyquix
+ */
+
+//    .d8888b. 88888888888 .d88888b.  8888888b.   888
+//   d88P  Y88b    888    d88P" "Y88b 888   Y88b  888
+//   Y88b.         888    888     888 888    888  888
+//    "Y888b.      888    888     888 888   d88P  888
+//       "Y88b.    888    888     888 8888888P"   888
+//         "888    888    888     888 888         Y8P
+//   Y88b  d88P    888    Y88b. .d88P 888          "
+//    "Y8888P"     888     "Y88888P"  888         888
+//
+//  DO NOT MODIFY THIS FILE!
+//  If you need a custom renderer, copy this file to php/custom/blocks/cards/default.tmpl.php and modify it there
+//  You may also create custom renderer for specific presets, by copying this file to /php/custom/blocks/map/{preset}.php
+
 //while this might be redundant for the main map block this is important for filters
 $map_settings = \lqx\blocks\get_settings('map', null, $s['preset'], $s['style']);
 // do we need to map the posts' data to match the expected structure of the map?
@@ -52,7 +76,7 @@ foreach($c as $item) {
 if ($s['show_map'] == 'y'):
 ?>
 <section
-	id="<?= esc_attr($s['anchor']) ?>"
+	id="<?= esc_attr($s['hash']) ?>"
 	class="lqx-block-map <?= esc_attr($s['class']) ?>" data-settings="<?= esc_attr(json_encode($map_settings)) ?>" data-items="<?= esc_attr(json_encode($processed_items)) ?>">
 <div class="map-wrapper">
 	<?php require \lqx\blocks\get_template('map', $s['preset'], 'map'); ?>
